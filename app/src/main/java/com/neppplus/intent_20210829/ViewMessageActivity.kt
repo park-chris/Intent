899ddd823ed25e8896e81d6f1f025325b82f7c0e
박수정 <zxzx4342@naver.com>
2021-08-29 15:34:51 +0900

    첨부해준 데이터를 받아서 -> UI에 반영

--- app/src/main/java/com/neppplus/intent_20210829/ViewMessageActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210829/ViewMessageActivity.kt
@@ -2,10 +2,22 @@ package com.neppplus.intent_20210829
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
+
+//        화면에 들어올때 첨부된 데이터를 꺼내서 => 변수에 저장
+
+        val receivedMessage = intent.getStringExtra("inputMessage")
+
+
+//        messageTxt의 text속성에 => 저장해둔 내용을 대입. (UI - set)
+
+        messageTxt.text = receivedMessage
+
+
     }
 }
