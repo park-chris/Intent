c3ea1fcc94a45f25ff08173589dbf67e69f5795d
박수정 <zxzx4342@naver.com>
2021-08-29 15:54:39 +0900

    Intent(3) 연습 UI - 닉네임 변경 화면 작성

---
+++ app/src/main/java/com/neppplus/intent_20210829/EditNicknameActivity2.kt
@@ -0,0 +1,11 @@
+package com.neppplus.intent_20210829
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname2)
+    }
+}
